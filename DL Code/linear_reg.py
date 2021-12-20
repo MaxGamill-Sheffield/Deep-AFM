3525da9451e9357fc3ccdf73113ad2707211b955
Max Gamill <mcgamill1@sheffield.ac.uk>
2021-12-20 17:01:11 +0000

    Linear regression data normalisation added

--- DL Code/linear_reg.py
+++ DL Code/linear_reg.py
@@ -28,8 +28,16 @@ def output(design_matrix, y):
 
 class basis_func():
     
-    def polynomial(x, n):
-        return x**n
+    def polynomial(x, num_basis=4, data_limits=[-1., 1.]):
+        "Polynomial basis"
+        centre = data_limits[0]/2. + data_limits[1]/2.
+        span = data_limits[1] - data_limits[0]
+        z = (x / x.mean()) * span + centre
+        z = z.reshape(len(z),1)
+        Phi = np.zeros((x.shape[0], num_basis))
+        for i in range(num_basis):
+            Phi[:, i:i+1] = z**i
+        return Phi
     
     def sigmoid(x, s=1):
         1/(1+np.exp(-1*(x-np.mean(x))/s))
@@ -56,50 +64,36 @@ def get_spline_jsons(path):
 #------- In Helper Scripts -------
 
 #------- Should be in Helper Scripts graphical -------
-def plot_poly_overlays(x, y, M):
+def plot_overlays(x, x_pred, y, title=None):
     fig, ax = plt.subplots(1,2,figsize=(10,5))
-    phi = np.ones((y.shape[0], M))
-    for m in range(M-1):
-        phi[:, m+1] = basis_func.polynomial(x, M)
     
-    phi = np.array([basis_func.polynomial(x, n) for n in range(M+1)]).T
-    ml_out, var_out = output(phi, y)
-    
-    fig.suptitle('ML Gaussian splining with Polynomial Degree M=%i'%M)
-    label = 'ML Gaussian'
-    ax[0].plot(x,y, label='TopoStats')
-    ax[0].plot(x,ml_out, label=label)
-    ax[1].plot(x,y, label='TopoStats')
-    ax[1].plot(x,ml_out, label=label)
+    fig.suptitle(title)
+    ax[0].plot(x, y, label='TopoStats Fit')
+    ax[0].plot(x, x_pred, label='ML Fit')
+    ax[1].plot(x, y, label='TopoStats Fit')
+    ax[1].plot(x, x_pred, label='ML Fit')
     ax[1].imshow(im, origin='upper', cmap='gray')
     ax[0].legend()
 
-
-def plot_exp_overlays(x, y, M):    
-    fig, ax = plt.subplots(1,2,figsize=(10,5))
-    phi = np.ones((y.shape[0], len(M)))
-    for m in M:
-        phi[:, m+1] = basis_func.exp(x, m)
-    
-    phi = np.array([basis_func.exp(x, m) for m in M]).T
-    ml_out = output(phi, y)
-    
-    fig.suptitle('ML Exp splining with sigma=%i, mu=%i'%(m,1))
-    label = 'ML Exp'
-    ax[0].plot(x,y, label='TopoStats')
-    ax[0].plot(x,ml_out, label=label)
-    ax[1].plot(x,y, label='TopoStats')
-    ax[1].plot(x,ml_out, label=label)
-    ax[1].imshow(im, origin='upper', cmap='gray')
-    ax[0].legend()
 #------- Should be in Helper Scripts graphical -------
 
 
 ts_path = "/Users/Maxgamill/Desktop/Uni/PhD/TopoStats/"
 splines = get_spline_jsons(ts_path + 'data/')
-im = io.imread(ts_path+'data/Processed/20160722_339_REL_15ng_434rep_80ng_PLL.077_ZSensor_processed_grey.tif')
+im = io.imread(ts_path+'data/Processed/20161024_339_LIN_6ng_434rep_8ng_PLL.026_ZSensor_processed_grey.tif')
 
-lin = splines['20160722_339_REL_15ng_434rep_80ng_PLL.077']['Splines']['2']
+lin = splines['20161024_339_LIN_6ng_434rep_8ng_PLL.026']['Splines']['1']
+
 x = np.asarray(lin['x_coord'])
-#X = np.column_stack([np.ones(len(x)), x]) #including bias
 y = np.asarray(lin['y_coord'])
+
+M=6
+
+basis = basis_func.polynomial(x,M)
+pred, var = output(basis, y)
+title='Polynomial Degree = %i'%M
+plot_overlays(x, pred, y, title)
+
+plt.imshow(im, cmap='gray')
+plt.legend()
+plt.show()
